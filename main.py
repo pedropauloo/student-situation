7d802a7bb75491a135184ba3d899961be67302d1
Pedro Paulo <pedropaulolucas@hotmail.com>
2025-07-02 20:05:03 -0300

    feat: adicionado algoritmo para ler pastas

--- main.py
+++ main.py
@@ -1,40 +1,65 @@
 import pandas as pd
+import os
 
-def get_matriculas():
-    matriculas = pd.read_csv("./dados/matriculas/matriculas.csv", sep=';', encoding='utf-8')
+def get_csv_from(pasta_csv):
+    dfs = []
+    arquivos = [f for f in os.listdir(pasta_csv) if f.endswith('.csv')]
+
+    if not arquivos:
+        print("Nenhum arquivo .csv encontrado na pasta.")
+        return pd.DataFrame()
+
+    for arquivo in arquivos:
+        caminho_completo = os.path.join(pasta_csv, arquivo)
+        try:
+            df = pd.read_csv(caminho_completo, sep=';', encoding='utf-8')
+            dfs.append(df)
+        except Exception as e:
+            print(f"Erro ao ler {arquivo}: {e}")
+
+    df_concatenado = pd.concat(dfs, ignore_index=True)
+    return df_concatenado
+
+def get_dados_pessoais():
+    dados_pessoais = get_csv_from("./dados/dados_pessoais_discentes")
+    dados_pessoais.columns = dados_pessoais.columns.str.strip().str.lower()
+    dados_pessoais['id_discente'] = dados_pessoais['id_discente'].astype(str).str.strip().str.lower()
+    
+    return dados_pessoais
+
+def get_matriculas(dados_pessoais):
+    matriculas = get_csv_from("./dados/matriculas")
     matriculas.columns = matriculas.columns.str.strip().str.lower()
-
+    
     matriculas['discente'] = matriculas['discente'].astype(str).str.strip().str.lower()
     matriculas.rename(columns={"discente": "id_discente"}, inplace=True)
+    
     matriculas["media_final"] = matriculas["media_final"].str.replace(",", ".")
     matriculas["media_final"] = pd.to_numeric(matriculas["media_final"], errors="coerce")
+    
     matriculas["numero_total_faltas"] = pd.to_numeric(matriculas["numero_total_faltas"], errors="coerce")
+    
     matriculas["reposicao"] = matriculas["reposicao"].map({"True": 1, "False": 0})
     matriculas["reposicao"] = pd.to_numeric(matriculas["reposicao"], errors="coerce").fillna(0)
+    
     matriculas = matriculas[matriculas['id_discente'].isin(dados_pessoais['id_discente'])]
 
     return matriculas
 
-def get_dados_pessoais():
-    dados_pessoais = pd.read_csv("./dados/dados_pessoais_discentes/dados_pessoais_discentes.csv", sep=';', encoding='utf-8')
-    dados_pessoais.columns = dados_pessoais.columns.str.strip().str.lower()
-    dados_pessoais['id_discente'] = dados_pessoais['id_discente'].astype(str).str.strip().str.lower()
-
-    return dados_pessoais
-
-def get_situacoes():
-    situacoes = pd.read_csv("./dados/situacao_discentes/situacao_discentes.csv", sep=';', encoding='utf-8')
+def get_situacoes(dados_pessoais):
+    situacoes = get_csv_from("./dados/situacao_discentes")
     situacoes.columns = situacoes.columns.str.strip().str.lower()
     situacoes['id_discente'] = situacoes['id_discente'].astype(str).str.strip().str.lower()
+    
     situacoes = situacoes[situacoes['id_discente'].isin(dados_pessoais['id_discente'])]
-
+    
     return situacoes
 
 def run():
-    matriculas = get_matriculas()
     dados_pessoais = get_dados_pessoais()
-    situacoes = get_situacoes()
-    
+    matriculas = get_matriculas(dados_pessoais)
+    situacoes = get_situacoes(dados_pessoais)
+
     df = pd.merge(dados_pessoais, matriculas, on='id_discente', how='inner')
     df = pd.merge(df, situacoes, on='id_discente', how='inner')
 
@@ -42,7 +67,6 @@ def run():
     df = df.sort_values(by=['id_discente','id_turma', 'unidade'])
 
     df.to_csv("dados_completos.csv", index=False, sep=';', encoding='utf-8')
-    
 
 run()
 
@@ -62,5 +86,3 @@ run()
 # 2. remover atributos desnecessários
 # 3. criar atributos necessários
 # 4. treinar modelo
-
-# meu id 8514817b733fca321fabc5d81b146c21
